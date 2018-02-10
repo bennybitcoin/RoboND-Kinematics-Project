9b6913af18e6f7a9c059fd709739851f0823d203
robond <bpbamberg@gmail.com>
2018-02-10 12:59:57 -0800

    submission #3

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -39,18 +39,14 @@ def handle_calculate_IK(req):
     # Joint angle symbols
     q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8') #theta_i
 
-    #Variables for Rotation Matrix
-    r, p, y = symbols('r p y')
-    #x, y, z = symbols('x y z')
-
    ###Kuka KR210###
    #DH Parameters
-    DH = {alpha0: 0,     a0: 0,      d1: 0.75,   q1:0,
+    DH = {alpha0: 0,     a0: 0,      d1: 0.75,
          alpha1: -pi/2, a1: 0.35,   d2: 0,     q2: q2-pi/2,
-         alpha2: 0,     a2: 1.25,   d3: 0,  q3: q3,
-         alpha3: -pi/2, a3: -0.054, d4: 1.50,   q4: q4,
-         alpha4: pi/2,  a4: 0,      d5: 0,  q5: q5,
-         alpha5: -pi/2, a5: 0,      d6: 0,  q6: q6,
+         alpha2: 0,     a2: 1.25,   d3: 0,
+         alpha3: -pi/2, a3: -0.054, d4: 1.50,
+         alpha4: pi/2,  a4: 0,      d5: 0,
+         alpha5: -pi/2, a5: 0,      d6: 0,
          alpha6: 0,     a6: 0,      d7: 0.303,  q7: 0}
    #DH test parameters
    # DH_test = {alpha0: 0,     a0: 0,      d1: 0.75,  q1:0,
@@ -64,12 +60,11 @@ def handle_calculate_IK(req):
     #
     # Define Modified DH Transformation matrix function
     def Trans_Matrix(alpha, a, d, q):
-    	TF = Matrix([[cos(q),  -sin(q),    0,  a],
-    		[sin(q)*cos(alpha),  cos(q)*cos(alpha),  -sin(alpha),    -sin(alpha)*d],
-    		[sin(q)*sin(alpha),   cos(q)*sin(alpha),  cos(alpha), cos(alpha)*d],
-    		[0,       0,      0,      1]])
-    	return TF
-
+        TF = Matrix([[cos(q),  -sin(q),    0,  a],
+            [sin(q)*cos(alpha),  cos(q)*cos(alpha),  -sin(alpha),    -sin(alpha)*d],
+            [sin(q)*sin(alpha),   cos(q)*sin(alpha),  cos(alpha), cos(alpha)*d],
+            [0,       0,      0,      1]])
+        return TF
 
     #
     # Create individual transformation matrices
@@ -81,7 +76,16 @@ def handle_calculate_IK(req):
     T5_6 = Trans_Matrix(alpha5, a5, d6, q6).subs(DH)
     T6_EE = Trans_Matrix(alpha6, a6, d7, q7).subs(DH)
 
-    T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
+    T0_EE = simplify(T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE)
+
+
+
+    #IK CODE================================================
+    # Extract rotation matrices from the transformation matrices
+    # End effector positions:
+    px = req.poses[x].position.x
+    py = req.poses[x].position.y
+    pz = req.poses[x].position.z
 
     #Set Roll Pitch and Yaw to end-effector postion
     (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
@@ -89,17 +93,21 @@ def handle_calculate_IK(req):
             req.poses[x].orientation.z, req.poses[x].orientation.w])
 
     # Create Rotation Matrices
+
+    #Variables for Rotation Matrix
+    r, p, y = symbols('r p y')
+
     Roll_rot = Matrix([[ 1,         0,          0],
                      [ 0, cos(r), -sin(r)],
                      [ 0, sin(r), cos(r)]])
 
     Pitch_rot = Matrix([[ cos(p),  0, sin(p)],
-    	               [          0,  1,          0],
-    	               [-sin(p),  0, cos(p)]])
+                       [          0,  1,          0],
+                       [-sin(p),  0, cos(p)]])
 
     Yaw_rot = Matrix([[ cos(y), -sin(y), 0],
-    	             [ sin(y),  cos(y), 0],
-    	             [        0,         0, 1]])
+                     [ sin(y),  cos(y), 0],
+                     [        0,         0, 1]])
 
     EE_rot = Yaw_rot * Pitch_rot * Roll_rot
 
@@ -125,15 +133,9 @@ def handle_calculate_IK(req):
 
     EE_rot = EE_rot.subs({'r': roll, 'p': pitch, 'y': yaw})
 
-    #
-    # Extract rotation matrices from the transformation matrices
-    # End effector positions:
-    px = req.poses[x].position.x
-    py = req.poses[x].position.y
-    pz = req.poses[x].position.z
 
     # Wrist center calculation
-    WC = Matrix([px, py, pz]) - (DH[d7]*1.23) * EE_rot[:,2]
+    WC = Matrix([px, py, pz]) - (0.303) * EE_rot[:,2]
 
 
     # Calculate joint angles using Geometric IK method
@@ -141,15 +143,16 @@ def handle_calculate_IK(req):
     WC_average = WC[0] * WC[0] + WC[1] * WC[1]
     # SSS triangle
     side_a = 1.501
-    side_b = sqrt(pow((sqrt(WC_average) - DH_table[a1]), 2) + pow((WC[2] - DH_table[d1]), 2))
+    side_b = sqrt(pow((sqrt(WC_average) - DH[a1]), 2) + pow((WC[2] - DH[d1]), 2))
     side_c = 1.25
 
     angle_a = acos((side_b * side_b + side_c * side_c - side_a * side_a) / (2 * side_b * side_c))
     angle_b = acos((side_a * side_a + side_c * side_c - side_b * side_b) / (2 * side_a * side_c))
-    angle_c = acos((side_a * side_a + side_b * side_b - side_c * side_c) / (2 * side_a * side_b))
+    #angle_c = acos((side_a * side_a + side_b * side_b - side_c * side_c) / (2 * side_a * side_b))
+    angle_d = atan2(WC[2] - DH[d1], sqrt(WC_average) - DH[a1])
 
-    theta2 = (pi / 2 - angle_a - atan2(WC[2] - DH_table[d1], sqrt(WC_average) - DH_table[a1])
-    theta3 = pi / 2 - (angle_b + atan2(WC[1], WC[0])) #-0.054m sag in link 4
+    theta2 = pi/2 - angle_a - angle_d
+    theta3 = pi/2 - (angle_b + 0.036) #-0.054m sag in link 4
     Rot_03 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
     Rot_03 = Rot_03.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
     Rot_36 = Rot_03.inv("LU") * EE_rot
